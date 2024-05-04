57ebc36212fee1889e4b04450fad5e760dc9093f
Camilo-neck <ccuello@unal.edu.co>
2024-05-04 12:27:52 -0500

    refactor: make model valid for spyne

--- src/models/search_response.py
+++ src/models/search_response.py
@@ -1,51 +1,27 @@
-class PostHighlight:
-	def __init__(self, name=None, lastname=None, nickname=None, description=None):
-		self.name = name
-		self.lastname = lastname
-		self.nickname = nickname
-		self.description = description
+from spyne import ComplexModel, Unicode, Integer, Array
 
-	@staticmethod
-	def from_map(map):
-		return PostHighlight(
-			name=map.get('name'),
-			lastname=map.get('lastname'),
-			nickname=map.get('nickname'),
-			description=map.get('description'),
-		)
+class PostHighlight(ComplexModel):
+	__namespace__ = 'highlight'
+	
+	name = Unicode
+	lastname = Unicode
+	nickname = Unicode
+	description = Unicode
 
+class PostRecommendation(ComplexModel):
+	__namespace__ = 'post'
+	
+	id = Integer
+	name = Unicode
+	type = Unicode
+	lastname = Unicode
+	nickname = Unicode
+	description = Unicode
+	picture = Unicode
 
-class PostRecommendation:
-	def __init__(self, id, name, type, lastname=None, nickname=None, description=None, picture=None):
-		self.id = id
-		self.name = name
-		self.type = type
-		self.lastname = lastname
-		self.nickname = nickname
-		self.description = description
-		self.picture = picture
+class SearchModel(ComplexModel):
+	__namespace__ = 'search'
+	
+	highlight = PostHighlight
+	post = PostRecommendation
 
-	@staticmethod
-	def from_map(map):
-		return PostRecommendation(
-			id=map['id'],
-			name=map['name'],
-			type=map['type'],
-			lastname=map.get('lastname'),
-			nickname=map.get('nickname'),
-			description=map.get('description'),
-			picture=map.get('picture'),
-		)
-
-
-class SearchModel:
-	def __init__(self, highlight, post):
-		self.highlight = highlight
-		self.post = post
-
-	@staticmethod
-	def from_map(map):
-		return SearchModel(
-			highlight=PostHighlight.from_map(map=map['highlight']),
-			post=PostRecommendation.from_map(map=map['post']),
-		)
